e72e346509247d1556bb9bab2522b05c4f2a758a
jaehyunlee <jaehyunlee25@gmail.com>
2021-10-27 02:04:58 +0900

    renew

---
+++ sqls/invite/teacher/newInvitation.sql
@@ -0,0 +1,21 @@
+insert into
+    invitation(
+        id,
+        type,
+        created_at,
+        updated_at,
+        class_id,
+        school_id,
+        phone,
+        role_name
+    )
+values(
+    uuid_generate_v1(),
+    ${type},
+    now(),
+    now(),
+    '${classId}',
+    '${schoolId}',
+    '${phone}',
+    '${roleName}'
+) returning id;
