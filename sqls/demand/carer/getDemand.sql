fbc107eadf43b65eef6cb05c70efdf6a1eb09d77
jaehyunlee <jaehyunlee25@gmail.com>
2021-11-22 15:27:00 +0900

    renew

--- sqls/demand/carer/getDemand.sql
+++ sqls/demand/carer/getDemand.sql
@@ -10,7 +10,11 @@ select
     c.id class_id, 
     c.name class_name, 
     c.description class_description
+    k.name kid_name,
+	k.birth kid_birth,
+	k.gender kid_gender
 from demand d
+    left join kid k on k.id = d.kid_id
     left join class c on c.id = d.class_id 
     join schools s on s.id = d.school_id 
 where 
