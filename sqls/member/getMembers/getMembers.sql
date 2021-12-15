0034c6e6215cecb655a270476d7c79f0643d18f8
jaehyunlee <jaehyunlee25@gmail.com>
2021-12-16 01:03:51 +0900

    renew

--- sqls/member/getMembers/getMembers.sql
+++ sqls/member/getMembers/getMembers.sql
@@ -2,10 +2,13 @@ select
 	m.id member_id,
 	m.nickname member_name,
 	m.image_id member_image_id,
+	c.id class_id, 
+	c.name class_name,
 	sr.grade member_grade
 from 
 	members m
 	left join school_roles sr on sr.id = m.school_role_id
+	left join class c on c.id = m.class_id
 where
     m.school_id = '${schoolId}'
     and sr.grade = ${grade};
