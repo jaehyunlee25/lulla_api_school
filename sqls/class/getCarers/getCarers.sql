9a34686bcbd8ba0f9123af3da7770695f0f44b3c
이재현 <jaehyunlee@ijaehyeon-ui-MacBookPro.local>
2021-12-16 23:33:16 +0900

    renew

--- sqls/class/getCarers/getCarers.sql
+++
@@ -1,9 +0,0 @@
-select
-    *
-from
-    members m
-    left join school_roles sr on sr.id = m.school_role_id
-where
-    m.school_id = '${schoolId}'
-    and m.class_id = '${classId}'
-    and sr.grade = 5;
