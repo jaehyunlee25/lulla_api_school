5e54b311cb9142d956b9c8016104a70efb5f9139
jaehyunlee <jaehyunlee25@gmail.com>
2021-12-20 14:20:46 +0900

    renew

--- sqls/member/getTeacher/getClasses.sql
+++ sqls/member/getTeacher/getClasses.sql
@@ -4,6 +4,8 @@ select
 from
     members m
     left join class c on c.id = m.class_id
+    left join school_roles sr on sr.id = m.school_role_id
 where
     m.user_id = '${userId}'
-    and m.school_id = '${schoolId}';
+    and m.school_id = '${schoolId}'
+    and sr.grade = 3;
